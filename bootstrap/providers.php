f8e1710615daea7aa852fd54bed5a41fc8d0e748
Mohammad Abdlrahman Sad <91919000+Mohammad-abdo@users.noreply.github.com>
2026-02-11 19:10:37 +0200

    update

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
-    App\Providers\AppServiceProvider::class,
+    // View must be registered first (API-only apps don't load it by default)
     Illuminate\View\ViewServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
 ];
